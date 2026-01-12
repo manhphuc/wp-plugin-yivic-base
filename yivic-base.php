feca79d3288ddc52cbcbb5396169a4ac59b95eb1
Phuc Nguyen <manhphucofficial@gmail.com>
2026-01-12 20:38:04 +0700

    chore: normalize project ignores and plugin metadata

--- yivic-base.php
+++ yivic-base.php
@@ -4,15 +4,15 @@
  * Description: The Base plugin for Theme and Plugin development. It requires ACF pro to work.
  * Version: 0.0.1
  * Author: Yivic
- * Author URI: http://www.yivic.com/wordpress-plugin-yivic-base
+ * Author URI: https://yivic.com/wp-plugin-yivic-base/
  * License: GPLv2 or later
- * Text Domain: yivic
+ * Text Domain: yivic-base
  * Domain Path: /languages/
  */
 
 use Yivic\Wp\YivicBase\Libs\WpApp;
 
-defined( 'YIVIC_BASE_PLUGIN_VER' ) || define( 'YIVIC_BASE_PLUGIN_VER', 0.3 );
+defined( 'YIVIC_BASE_PLUGIN_VER' ) || define( 'YIVIC_BASE_PLUGIN_VER', '0.0.1' );
 defined( 'YIVIC_BASE_PLUGIN_PATH' ) || define( 'YIVIC_BASE_PLUGIN_PATH', __DIR__ );
 defined( 'YIVIC_BASE_PLUGIN_FOLDER_NAME' ) || define( 'YIVIC_BASE_PLUGIN_FOLDER_NAME', 'yivic-base' );
 defined( 'YIVIC_BASE_PLUGIN_URL' ) || define( 'YIVIC_BASE_PLUGIN_URL', plugins_url( null, YIVIC_BASE_PLUGIN_PATH ) );
